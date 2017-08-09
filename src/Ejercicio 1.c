39b04094a0c000112e2f701d1d079234f47fe604
Gustavo Fabian Paredes Delaloye <lu2jgp@gmail.com>
2017-08-08 22:09:24 -0300

    Primera Carga... O sea... V 1.0
    Descripción extendida: Nada que agregar.

---
+++ src/Ejercicio 1.c
@@ -0,0 +1,31 @@
+/*
+1/2: Realizar un programa que permita ingresar una palabra y luego devuelva
+por consola si se repite la letra 's'.
+En caso de ser afirmativo, que también indique la cantidad de veces
+que se repite la letra.
+*/
+
+#include <stdio.h>
+
+int main ()
+{
+	
+	char palabra [21];
+	int i;
+	int contador = 0;
+		
+	printf("\nIngrese la palabra: ");
+	scanf("%s",palabra);
+	
+	for (i=0;i<20;i++)
+	{
+		if (palabra[i] == 's' || palabra[i] == 'S') contador++;
+	
+	}
+	
+	if (contador == 1) printf("\nLa letra `s` se encuentra repetida %d vez", contador);
+		else
+		if (contador > 1) printf("\nLa letra `s` se encuentra repetida %d veces", contador);
+			else
+			printf("\nLa letra 's' o 'S' no se encuentra en la palabra ingresada");
+}
